72664c99b1a04bce18353cc01d881cb5cf298189
Ladyka Alisa <ladykaalisa@gmail.com>
2024-04-26 09:58:11 +0300

    done test1

--- cypress/e2e/1-api tests/api-tests.cy.js
+++ cypress/e2e/1-api tests/api-tests.cy.js
@@ -3,26 +3,26 @@
 
 describe('API Tests', () => {
   it('should return 200 status code for GET /posts', () => {
-    cy.request('/posts').then((response) => {
+    cy.request('https://jsonplaceholder.typicode.com/posts').then((response) => {
       expect(response.status).to.eq(200);
     });
   });
 
   it('should return 200 status code for GET /users', () => {
-    cy.request('/users').then((response) => {
+    cy.request('https://jsonplaceholder.typicode.com/users').then((response) => {
       expect(response.status).to.eq(200);
     });
   });
 
   it('should return a specific user by ID', () => {
-    cy.request('/users/1').then((response) => {
+    cy.request('https://jsonplaceholder.typicode.com/users/1').then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.id).to.eq(1);
     });
   });
 
   it('should return the correct number of posts', () => {
-    cy.request('/posts').then((response) => {
+    cy.request('https://jsonplaceholder.typicode.com/posts').then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.length).to.eq(100); 
     });
@@ -31,7 +31,7 @@ describe('API Tests', () => {
   it('should return 200 status code for DELETE /posts/{id}', () => {
     cy.request({
       method: 'DELETE',
-      url: '/posts/1', 
+      url: 'https://jsonplaceholder.typicode.com/posts/1', 
     }).then((response) => {
       expect(response.status).to.eq(200);
     });
